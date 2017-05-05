b67a033afa6bb81cc4cbf57c58c67e826115318d
Dinh Khang Tran <khangtd.09@gmail.com>
2017-05-05 17:59:42 +0700

    commit

---
+++ src/components/Index.vue
@@ -0,0 +1,58 @@
+<template>
+    <q-layout>
+        <div slot="header" class="toolbar">
+            <button class="hide-on-drawer-visible" @click="$refs.drawer.open()">
+                <i>menu</i>
+            </button>
+            <q-toolbar-title :padding="2">
+                Hi Khang, you spent 20% the plan.
+            </q-toolbar-title>
+        </div>
+        <q-drawer ref="drawer">
+            <div class="list platform-delimiter">
+                <div class="list-header">
+                    Hi Khang,
+                </div>
+                <q-drawer-link icon="mail" to="/account">View Report</q-drawer-link>
+                <q-drawer-link icon="alarm" to="/alarms">View Payments</q-drawer-link>
+            </div>
+        </q-drawer>
+        <!--
+      Replace following "div" with
+      "<router-view class="layout-view">" component
+      if using subRoutes
+    -->
+        <div class="layout-view"></div>
+    </q-layout>
+</template>
+<script>
+import {
+    Utils
+} from 'quasar'
+import Router from '../router';
+export default {
+    data() {
+            return {
+                id: Utils.uid(),
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+        mounted() {
+            Router.push('/login');
+        },
+        beforeDestroy() {
+
+        },
+        components: {
+
+        }
+
+}
+</script>
+<style>
+</style>
---
+++ src/components/auth/Auth.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="fluid-wrapper flex justify-center">
+        <div class="auth-form row inline auto items-center justify-center self-center">
+            <div class="form-wrapper row auto wrap justify-center">
+                <!-- <div class="row justify-center text-center">
+                    <div class="auth-form-logo sm-width-3of3">
+                        <img src="../../assets/logo.png">
+                    </div>
+                </div> -->
+                <div class="row wrap width-2of5 justify-center text-center">
+                    <div class="auth-form-logo sm-width-3of3">
+                        <img src="../../assets/logo_text.png">
+                    </div>
+                </div>
+                <div class="auth-form-content width-2of5 sm-width-4of5 wrap row auto">
+                    <div class="greeting-wrapper row full-width rotateInDownLeft animated">
+                        <span class="greeting label pointing-down text-white">explore your FIPER</span>
+                    </div>
+                    <div class="floating-label large-gutter">
+                        <input required class="full-width" v-model="username">
+                        <label>Username</label>
+                    </div>
+                    <div class="floating-label large-gutter">
+                        <input type="password" required class="full-width" v-model="password">
+                        <label>Password</label>
+                    </div>
+                    <div class="row justify-center wrap">
+                        <button class="primary outline" @click="login()">Login</button>
+                        <button class="secondary" @click="login()">Register</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Router from "../../router"
+import { Toast } from 'quasar'
+
+export default {
+    data: function() {
+        return {
+            username: '',
+            password: ''
+        }
+
+    },
+    methods: {
+        login: function() {
+            // Take it easy first
+            if (this.username == "khangtd" && this.password == "khangtd") {
+                Router.push('/');
+            }
+            if (this.username.length == 0 || this.password.length == 0) {
+                const dialog = Toast.create.info({
+                    title: 'Warning',
+                    html: 'You need to fill the required fields.'
+                })
+            }
+        }
+    }
+
+}
+</script>
+<style>
+button {
+    margin: 40px 5px;
+}
+
+.auth-form button.outline {
+    color: white;
+}
+
+.auth-form span.greeting {
+    width: auto;
+    height: 50px;
+    margin-left: 0px;
+    background: transparent;
+    border: solid 1px;
+}
+
+.auth-form {
+    color: white!important;
+}
+
+.auth-form label,
+.auth-form input {
+    color: white!important;
+}
+
+.auth-form label {
+    font-size: 13px;
+}
+
+div.fluid-wrapper {
+    width: 100%;
+    height: 100%;
+    background: green;
+    position: absolute;
+}
+</style>
