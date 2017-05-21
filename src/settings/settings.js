04722e106018bdc6af6bab093e4b3cb8eddcffce
Dinh Khang Tran <khangtd.09@gmail.com>
2017-05-21 13:53:06 +0700

    fix bugs eror when display images

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 import Database from './database'
 var ROOT_PATH = path.resolve(__dirname,'../')
-var STATIC_URL = path.resolve(__dirname,'../statics')
+var STATIC_URL = 'statics/'
 export {
 	Database,
 	ROOT_PATH,
